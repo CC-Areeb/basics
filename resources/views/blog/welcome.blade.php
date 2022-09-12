d7d627a0bae4d49430d404e7c911abf802da3e11
areeb <areeb.malik@cooperativecomputing.com>
2022-09-12 21:00:35 +0500

    Blade directives

--- resources/views/blog/welcome.blade.php
+++ resources/views/blog/welcome.blade.php
@@ -1,37 +1,10 @@
 @extends('layouts.master')
 @section('content')
-    {{-- <table class="table text-center">
-        <thead>
-            <tr>
-                <th scope="id">Id</th>
-                <th scope="title">Title</th>
-                <th scope="excerpt">Summery</th>
-                <th scope="body">Content</th>
-                <th scope="minutes_to_read">Minutes to read</th>
-                <th scope="created_at">Create at</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">{{ $posts->id }}</th>
-                <td>{{ $posts->title }}</td>
-                <td>{{ $posts->excerpt }}</td>
-                <td>{{ $posts->body }}</td>
-                <td>{{ $posts->minutes_to_read }}</td>
-                <td>{{ $posts->created_at }}</td>
-            </tr>
-        </tbody>
-    </table> --}}
 
-    @forelse ($collection as $item)
-        
+    @forelse ($posts as $post)
+        {{ $loop->depth }}
     @empty
-        
+        <p>Nothing here to see</p>
     @endforelse
 
 @endsection
-
-
-
-
-
