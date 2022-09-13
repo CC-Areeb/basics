153fbf96cbdcae38430b83ebe1f800088e593b2e
areeb <areeb.malik@cooperativecomputing.com>
2022-09-13 12:32:05 +0500

    Eloquent model basics

--- resources/views/blog/welcome.blade.php
+++ resources/views/blog/welcome.blade.php
@@ -1,10 +1,4 @@
 @extends('layouts.master')
 @section('content')
-
-    @forelse ($posts as $post)
-        {{ $loop->depth }}
-    @empty
-        <p>Nothing here to see</p>
-    @endforelse
-
+    
 @endsection
